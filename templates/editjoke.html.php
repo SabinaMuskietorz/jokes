aac4c689ec9a6ddb2a410a70fd045f7778f1de5f
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-02-22 21:39:05 +0000

    first commit

--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -5,5 +5,7 @@
 	<input type="hidden" name="joke[id]" value="<?= $joke['id'] ?? ''?>"/>
 	<label for="joketext">Type your joke here:</label>
 	<textarea id="joketext" name="joke[joketext]" rows="3" cols="40" > <?= $joke['joketext'] ?? ''?> </textarea>
+	<label for="authorName">Type your name here:</label>
+	<input type="text" name="joke[authorName]" value="<?= $joke['authorName'] ?? ''?>"/>
 	<input type="submit" name="submit" value="Add" />
 </form>
