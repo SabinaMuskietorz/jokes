221e1269d3b5ebf8cccd268dd99c96ac1eea4f68
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-03-08 16:14:47 +0000

    first commit

--- public/index.php
+++ public/index.php
@@ -2,14 +2,20 @@
 require '../functions/loadTemplate.php';
 require '../dbconfig.php';
 require '../classes/DatabaseTable.php';
+require '../controllers/JokeController.php';
 
 $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+$jokeController = new JokeController($jokesTable);
 
 if ($_SERVER['REQUEST_URI'] !== '/') {
-require '../pages/' . ltrim(explode('?', $_SERVER['REQUEST_URI'])[0], '/') . '.php';
+    $functionName = ltrim(explode('?', $_SERVER['REQUEST_URI'])[0], '/') . '.php';
+    $page = $jokeController->$functionName();
 }
 else {
-    require '../pages/home.php';
+    $page = $jokeController->home();
 }
+
+$output = loadTemplate('../templates/' . $page['template'], $page['variables']);
+$title = $page['title'];
 
 require  '../templates/layout.html.php';
