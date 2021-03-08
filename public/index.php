1451eeda47639d0da70eb4fcebe7d6b74e151210
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-03-08 14:53:39 +0000

    first commit

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-require '../loadTemplate.php';
+require '../functions/loadTemplate.php';
 require '../dbconfig.php';
-require '../DatabaseTable.php';
+require '../classes/DatabaseTable.php';
 
 $title = 'Internet Joke Database';
 $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
 $jokes = $jokesTable->find('id', 1);
 
-$output = loadTemplate('../templates/home.html.php', ['joke' => $joke[0]]);
+require '../pages/' . $_GET['page'] . '.php';
 
 require  '../templates/layout.html.php';
