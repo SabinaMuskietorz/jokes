34040bc7baf277d1fc0863fea823dac74b4d0127
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-03-15 16:59:53 +0000

    first commit

---
+++ Ijdb/controllers/Routes.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ijdb;
+class Routes {
+    class Routes implements \CSY2028\Routes {
+        public function getPage($route) {
+ require '../database.php';
+ $jokesTable = new \CSY2028\DatabaseTable($pdo, 'joke', 'id');
+ $categoriesTable = new \CSY2028\DatabaseTable($pdo, 'category', 'id');
+ $controllers = [];
+ $controllers['joke'] = new \Ijdb\Controllers\Joke($jokesTable);
+ $controllers['category'] = new \Ijdb\Controllers\Category($categoriesTable);
+ if ($route == '') {
+ $page = $controllers['joke']->home();
+ }
+ else {
+ list($controllerName, $functionName) = explode('/', $route);
+ $controller = $controllers[$controllerName];
+ $page = $controller->$functionName();
+ }
+ return $page;
+ }
+}
+}
+?>
--- public/index.php
+++ public/index.php
@@ -1,29 +1,6 @@
 <?php
-namespace CSY2028;
-namespace Ijdb\Controllers;
-require '../functions/loadTemplate.php';
-require '../dbconfig.php';
-/*require '../classes/DatabaseTable.php';
-require '../controllers/JokeController.php';
-require '../Controllers/CategoryController.php';*/
-
-$jokesTable = new \CSY2028\DatabaseTable($pdo, 'joke', 'id');
-$categoriesTable = new \CSY2028\DatabaseTable($pdo, 'category', 'id');
-
-$controllers = [];
-$controllers['joke'] = new Ijdb\Controllers\Joke($jokesTable);
-$controllers['category'] = new Ijdb\Controllers\Category($categoriesTable);
-
-$route = ltrim(explode('?', $_SERVER['REQUEST_URI'])[0], '/');
-if ($route == '') {
-    $page = $controllers['joke']->home();
-}
-else {
-    list($controllerName, $functionName) = explode('/', $route);
-    $controller = $controllers[$controllerName];
-    $page = $controller->$functionName();
-}
-$output = loadTemplate('../templates/' . $page['template'], $page['variables']);
-$title = $page['title'];
-require '../templates/layout.html.php';
+require '../autoload.php';
+$routes = new \Ijdb\Routes();
+$entryPoint = new \CSY2028\EntryPoint($routes);
+$entryPoint->run();
 ?>
